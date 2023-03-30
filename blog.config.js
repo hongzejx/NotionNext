83ac31673c86f2fbb08b0169b598c9f9df0cc72a
hongzejx <63963470+hongzejx@users.noreply.github.com>
2023-03-30 08:53:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '鸿泽机械', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专业印刷包装设备改造/销售', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.hongzejx.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '分切机, 复合机,印刷机,制袋机,干式复合机,凹版印刷机,复合制袋机,二手分切机,二手凹印机,二手凹版印刷机,二手复合机,二手干式复合机,二手制袋机,塑料包装设备', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '分切机, 复合机,印刷机,制袋机,干式复合机,凹版印刷机,二手分切机,二手凹版印刷机,二手复合机,二手干式复合机,二手制袋机,塑料包装设备', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
